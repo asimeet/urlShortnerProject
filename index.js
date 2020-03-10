c7e9f5f4dc2866f24850961f7b41e19b54d6f244
Asimeet Padhee <asimeet@onco.com>
2020-03-10 19:55:03 +0530

    changes-for-cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const bodyParser = require('body-parser');
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://13.58.109.119:4400"); 
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });

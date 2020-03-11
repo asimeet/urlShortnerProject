5bfef457fa3042779370632d52762dde4abe5908
Asimeet Padhee <asimeet@onco.com>
2020-03-11 14:51:13 +0530

    more-validation

--- src/routers/shorten-url.js
+++ src/routers/shorten-url.js
@@ -6,6 +6,13 @@ class ShortenUrlRouter extends RouterBase{
     }
     prepareResponse(){
         this.router.use('/', (req, res) => {
+            if(!req.body || !req.body.longUrl){
+                throw new Error(`Missing Request Data - body.longUrl`);
+            }
+            let validUrl = req.body.longUrl.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g);
+            if(!validUrl || validUrl.length == 0){
+                throw new Error('Invalid URL');
+            }
             let serviceInst = new urlShortner(req.body.longUrl);
             let result = serviceInst.shortenUrl();
             res.send(JSON.stringify(result));

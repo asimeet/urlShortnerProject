22f73b902f166415f2e272e9db2c730476f9405a
Asimeet Padhee <asimeet@onco.com>
2020-10-15 01:42:18 +0530

    home-css

--- src/routers/home-css.js
+++ src/routers/home-css.js
@@ -1,13 +1,17 @@
 
 const RouterBase =  require('../lib/router-base');
 const fs = require("fs");
+
 class HomeJsRouter extends RouterBase{
     constructor(req,res){
         super();
     }
     setUpRoutes(){
         this.router.use('/',(req,res)=>{
-            let data = fs.readFileSync("../assets/home.css");
+            let dirname = __dirname;
+            dirname = dirname.split("src/routers")[0];
+            let filepath = dirname + "src/assets/home.css";
+            let data = fs.readFileSync(filepath,"utf8");
             res.type('.css');
             res.send(`${data}`);
         });
